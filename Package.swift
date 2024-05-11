dbc3767179d0f2e16856f51d18274ed903a534e6
Tao <12044982+94haox@users.noreply.github.com>
2024-05-11 11:22:16 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Toast",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v11),
         .tvOS(.v13),
         .visionOS(.v1)
     ],
